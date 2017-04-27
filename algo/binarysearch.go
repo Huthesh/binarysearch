1db1ba3ac40eac5fbd4a500a35c8f6359798b3a6
hutheshk <hutheshk@gmail.com>
2017-04-27 20:01:30 +0530

    Changed if statement to switch case

--- algo/binarysearch.go
+++ algo/binarysearch.go
@@ -4,25 +4,6 @@ import (
 
 )
 
-/*func BinarySearch(array []int,key int) (result bool){	
-		startIndex:=0
-		length:=len(array)
-		lastIndex:=length-1
-		for startIndex<=lastIndex {
-			mid:=(startIndex+lastIndex)/2
-			if(array[mid]==key){
-				return true;
-			}
-			if(key>array[mid]){
-				startIndex=mid+1
-			} else if(key<array[mid]) {
-				lastIndex=mid-1
-			}
-			
-		}
-		return false
-}*/
-
 func BinarySearch(array []int,key int) (result bool){	
 		startIndex:=0
 		length:=len(array)
