2fdf217e590958ef6c46c9dd5d20f5f31f796447
hutheshk <hutheshk@gmail.com>
2017-04-27 19:43:22 +0530

    Committing the logic

---
+++ main.go
@@ -0,0 +1,14 @@
+package main 
+
+import (
+	"binarysearch/algo"
+	"fmt"
+)
+
+func main() {
+	array:=[]int{2, 3, 5, 7, 11, 13}
+	
+	fmt.Println(algo.BinarySearch(array,7))
+	
+	fmt.Println(algo.BinarySearch(array,1))
+}
